dfc41aac189fb5f31dc5ea85361eb63c3a117031
smithbm2 <smithbm2@rose-hulman.edu>
2019-01-24 09:47:17 -0500

    Oi!

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -72,7 +72,7 @@ def yes(a,b):
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
